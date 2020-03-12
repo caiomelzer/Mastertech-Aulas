7aca21387ad7f0794e547291976743c9ee4df995
a2 <a2@localhost.localdomain>
2020-03-12 11:36:54 -0300

    Adicionado ConfiServer

---
+++ configserver/src/test/java/com/melzer/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melzer.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
